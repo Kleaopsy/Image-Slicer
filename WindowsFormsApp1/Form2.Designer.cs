863ecafc8a58c4ad8e9857e0d97dc4587dfdce43
Kutay Gelen <75790484+Kleaopsy@users.noreply.github.com>
2022-03-22 15:39:32 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.resimSeçToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.diziToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 200);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // resimSeçToolStripMenuItem
+            // 
+            this.resimSeçToolStripMenuItem.Name = "resimSeçToolStripMenuItem";
+            this.resimSeçToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.resimSeçToolStripMenuItem.Text = "Select Image";
+            this.resimSeçToolStripMenuItem.Click += new System.EventHandler(this.resimSeçToolStripMenuItem_Click);
+            // 
+            // diziToolStripMenuItem
+            // 
+            this.diziToolStripMenuItem.Name = "diziToolStripMenuItem";
+            this.diziToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.diziToolStripMenuItem.Text = "Save Location";
+            this.diziToolStripMenuItem.Click += new System.EventHandler(this.CikartilacakDiziToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resimSeçToolStripMenuItem,
+            this.diziToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(232, 47);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Visible = false;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(232, 87);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(216, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Row";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Column";
+            this.label2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Böl";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 236);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Resim Böl";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolStripMenuItem resimSeçToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem diziToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ResimSeç = new OpenFileDialog();
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            menuStrip1.Items[1].Enabled = false;
+            ResimSeç.Title = "Select Image";
+            ResimSeç.Filter = "Image|*.jpg;*.jpeg;*.png";
+            ResimSeç.RestoreDirectory = false;
+            ResimSeç.InitialDirectory = @"C:\Users\LAB4\Desktop";            
+        }
+
+        private void resimSeçToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ResimSeç.ShowDialog() == DialogResult.OK)
+            {        
+                
+
+                groupBox1.Controls.Clear();
+                menuStrip1.Items[1].Enabled = true;
+                PictureBox pbx = new PictureBox();
+                pbx.Name = "pbx1";
+                pbx.Size = new Size(200,200);
+                pbx.Location = new Point(0,0);
+                pbx.BackgroundImageLayout = ImageLayout.Zoom;
+                pbx.BackgroundImage = Image.FromFile(ResimSeç.FileName);
+                Resim = Image.FromFile(ResimSeç.FileName) as Bitmap;
+                groupBox1.Controls.Add(pbx);
+
+                numericUpDown1.Visible = true;
+                numericUpDown2.Visible = true;
+                label1.Visible = true;
+                label2.Visible = true;
+                button1.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Select an image!");
+            }
+        }
+
+        int a;
+        FolderBrowserDialog Dizi = new FolderBrowserDialog();
+        private void CikartilacakDiziToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (a==0)
+            {
+                MessageBox.Show("we'll create 'newImages' folder ");
+                a++;
+            }
+            if (Dizi.ShowDialog()==DialogResult.OK)
+            {
+                label1.Visible = true;
+                label2.Visible = true;
+                numericUpDown1.Visible = true;
+                numericUpDown2.Visible = true;
+                button1.Visible = true;
+            }
+        }
+
+        int X, Y;
+        int ResimX, ResimY;
+        
+        Bitmap Parcabitmap;
+        void Parçala(int g, int y)
+        {
+            ResimX = Resim.Width; ResimY = Resim.Height;
+            İmage.Clear();
+            
+            for (int f1 = 1; f1 <= g*y; f1++)
+            {
+               
+                    Rectangle Kesme = new Rectangle(X, Y, ResimX / g, ResimY / y);
+                
+                    Parcabitmap=Resim.Clone(Kesme,Resim.PixelFormat);
+                
+                İmage.Add(Parcabitmap);
+                    X += ResimX / g;
+                if (f1%g==0)
+                {
+                    Y += ResimY/y;
+                    X = 0;
+                }
+            }
+            X = 0;
+            Y = 0;
+
+            Directory.CreateDirectory(Dizi.SelectedPath+@"\newImages");
+            for (int i=1;i<=g*y;i++)
+            {
+                İmage[i - 1].Save(Dizi.SelectedPath + @"\newImages\" + i.ToString() + ".jpg");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Parçala(Convert.ToInt32(numericUpDown1.Value), Convert.ToInt32(numericUpDown2.Value));
+        }    
+
+
+        Bitmap Resim;
+        List<Image> İmage = new List<Image>();
+    }
+}
